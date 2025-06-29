86e639e8a48de98409dbe0198ea2de74399132b2
Egehan <egehanavcu@gmail.com>
2025-06-30 00:11:16 +0300

    Added coefficients for score calculation algorithm

--- core/config.py
+++ core/config.py
@@ -7,5 +7,7 @@ class Settings:
     JWT_SECRET = os.getenv("JWT_SECRET")
     LOCATION_COUNT = int(os.getenv("LOCATION_COUNT"))
     QUESTION_DURATION = int(os.getenv("QUESTION_DURATION"))
+    ALPHA = float(os.getenv("ALPHA_COEFFICIENT"))
+    BETA = float(os.getenv("BETA_COEFFICIENT"))
 
 settings = Settings()
