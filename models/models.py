841347b5f0ff910aca265ee073cff5e8b9364c13
Egehan <egehanavcu@gmail.com>
2025-06-29 21:40:58 +0300

    Created database structure

---
+++ models/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    score = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    show_name = Column(Boolean, default=False)
+
+class Location(Base):
+    __tablename__ = "locations"
+    id = Column(Integer, primary_key=True)
+    image_url = Column(String)
+    lat = Column(Float)
+    lng = Column(Float)
+
+class Guess(Base):
+    __tablename__ = "guesses"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    location_id = Column(Integer, ForeignKey("locations.id"))
+    guessed_lat = Column(Float, nullable=True)
+    guessed_lng = Column(Float, nullable=True)
+    distance = Column(Float, nullable=True)
+    started_at = Column(Float, nullable=True)
+    time_taken = Column(Float, nullable=True)
+    score = Column(Integer, nullable=True)
