b218ad03175dda039136c6ca9c780e4fee2179cb
Egehan <egehanavcu@gmail.com>
2025-06-30 00:13:26 +0300

    Added score calculation function

--- services/game_logic.py
+++ services/game_logic.py
@@ -2,6 +2,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql.expression import func
 from models.models import Location
 from core.config import settings
+import math
 
 def get_random_locations(db: Session):
     return db.query(Location).order_by(func.random()).limit(settings.LOCATION_COUNT).all()
+
+def calculate_score(distance, duration):
+    return math.exp(-settings.ALPHA * distance) * math.exp(-settings.BETA * duration) * 1000
