d9754f9ba5ed71b8ac72dfbe524375ca555d60e2
Egehan <egehanavcu@gmail.com>
2025-06-29 23:47:58 +0300

    Added QUESTION_DURATION config

--- core/config.py
+++ core/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 class Settings:
     JWT_SECRET = os.getenv("JWT_SECRET")
     LOCATION_COUNT = int(os.getenv("LOCATION_COUNT"))
+    QUESTION_DURATION = int(os.getenv("QUESTION_DURATION"))
 
 settings = Settings()
