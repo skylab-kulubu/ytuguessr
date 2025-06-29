21282e2b4419e77d7b011a2cc55e04b7cd42566d
Egehan <egehanavcu@gmail.com>
2025-06-29 23:40:27 +0300

    Added Haversine formula function

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,20 @@
+import math
 import jwt
 import time
 from fastapi import HTTPException
 from core.config import settings
 from models.models import User
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371
+    dLat = math.radians(lat2 - lat1)
+    dLon = math.radians(lon2 - lon1)
+    a = (math.sin(dLat/2)**2 +
+         math.cos(math.radians(lat1)) *
+         math.cos(math.radians(lat2)) *
+         math.sin(dLon/2)**2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
 
 def create_jwt(email):
     return jwt.encode({"email": email, "timestamp": time.time()}, settings.JWT_SECRET, algorithm="HS256")
