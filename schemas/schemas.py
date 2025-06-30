869a11d1144e7f60711e8dbb3a5cac95775a37c7
Egehan <egehanavcu@gmail.com>
2025-06-30 14:43:44 +0300

    Created /api/game/guess route schema

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, EmailStr
 class StartGameRequest(BaseModel):
     school_mail: EmailStr
     show_name: bool = True
+
+class GuessRequest(BaseModel):
+    latitude: float
+    longitude: float
