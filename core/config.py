67198124e1b76f4dda67c9bada6e6826ffa58213
Egehan <egehanavcu@gmail.com>
2025-07-01 16:38:58 +0300

    Changed DATABASE_URL config

--- core/config.py
+++ core/config.py
@@ -4,12 +4,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD  = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = int(os.getenv("POSTGRES_PORT"))
     JWT_SECRET = os.getenv("JWT_SECRET")
     LOCATION_COUNT = int(os.getenv("LOCATION_COUNT"))
     QUESTION_DURATION = int(os.getenv("QUESTION_DURATION"))
     ALPHA = float(os.getenv("ALPHA_COEFFICIENT"))
     BETA = float(os.getenv("BETA_COEFFICIENT"))
     LEADERBOARD_PAGE_SIZE = int(os.getenv("LEADERBOARD_PAGE_SIZE"))
+
+    @property
+    def DATABASE_URL(self):
+        return (
+            f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
 settings = Settings()
