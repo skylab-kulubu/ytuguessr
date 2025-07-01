39bb84f98aef15af344ff140b62acf34082e454b
Egehan <egehanavcu@gmail.com>
2025-07-01 16:16:11 +0300

    Migrated database to PostgreSQL

--- core/database.py
+++ core/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./game.db"
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_size=20,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_pre_ping=True,
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
