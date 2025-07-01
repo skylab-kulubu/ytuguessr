eaa03aa0dd8efe527c6c3a851d70628629a13e65
Egehan <egehanavcu@gmail.com>
2025-07-01 14:37:29 +0300

    Minor improvement in score calculation

--- services/game_logic.py
+++ services/game_logic.py
@@ -8,4 +8,9 @@ def get_random_locations(db: Session):
     return db.query(Location).order_by(func.random()).limit(settings.LOCATION_COUNT).all()
 
 def calculate_score(distance, duration):
+    if duration <= settings.QUESTION_DURATION / 12:
+        duration = 0
+    else:
+        duration -= settings.QUESTION_DURATION / 12
+
     return math.exp(-settings.ALPHA * distance) * math.exp(-settings.BETA * duration) * 1000
