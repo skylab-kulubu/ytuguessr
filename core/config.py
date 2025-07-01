5427b604aeb23dc83309ed48e8757463d9d1b468
Egehan <egehanavcu@gmail.com>
2025-07-01 23:51:13 +0300

    Added ADMIN_SECRET config for admin panel access

--- core/config.py
+++ core/config.py
@@ -9,6 +9,7 @@ class Settings:
     POSTGRES_DB = os.getenv("POSTGRES_DB")
     POSTGRES_HOST = os.getenv("POSTGRES_HOST")
     POSTGRES_PORT = int(os.getenv("POSTGRES_PORT"))
+    ADMIN_SECRET = os.getenv("ADMIN_SECRET")
     JWT_SECRET = os.getenv("JWT_SECRET")
     LOCATION_COUNT = int(os.getenv("LOCATION_COUNT"))
     QUESTION_DURATION = int(os.getenv("QUESTION_DURATION"))
